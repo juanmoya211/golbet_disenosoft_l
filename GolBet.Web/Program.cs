e7846ad14873e4ee2c0ac5ea6bdfdf4eeed72363
JUAN DAVID MOYA CORRALES <juanmoya1138806@correo.itm.edu.co>
2026-09-19 15:54:18 -0500

    Add services layer, DTOs, AutoMapper and match board (Module 4)

--- GolBet.Web/Program.cs
+++ GolBet.Web/Program.cs
@@ -1,6 +1,8 @@
 using GolBet.Repositories.Data;
 using GolBet.Repositories.Implementations;
 using GolBet.Repositories.Interfaces;
+using GolBet.Services.Implementations;
+using GolBet.Services.Mapping;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,6 +19,13 @@ builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepositor
 
 // Specific repositories
 builder.Services.AddScoped<IMatchRepository, MatchRepository>();
+
+
+// AutoMapper: scans the assembly containing MappingProfile for all profiles
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+// Business services
+builder.Services.AddScoped<IMatchService, MatchService>();
 
 
 var app = builder.Build();
